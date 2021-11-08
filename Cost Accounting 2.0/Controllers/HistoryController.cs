a340f1f97a9dfc04f4620603909889201ccf95d2
Dmitry-Loginov <interestingfacts223@gmail.com>
2021-11-09 01:50:10 +0300

    add history view

---
+++ Cost Accounting 2.0/Controllers/HistoryController.cs
@@ -0,0 +1,24 @@
+﻿using Cost_Accounting_2._0.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cost_Accounting_2._0.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HistoryController : Controller
+    {
+        ApplicationContext Context { get; set; }
+        public HistoryController(ApplicationContext context)
+        {
+            Context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<History> histories = await Context.Histories.Include(h => h.User).ToListAsync();
+            return View(histories);
+        }
+    }
+}
